e92717b24b559899e3abb45e1c06d60bb37f1832
Stean <stean@gmx.org>
2013-04-04 05:32:32 +0200

    moved temporary code from ezmlm.php to action.php and ajusted spaces

--- action.php
+++ action.php
@@ -14,7 +14,6 @@ if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 require_once DOKU_PLUGIN.'action.php';
-require_once 'ezmlm.php';
 
 class action_plugin_restrictedregistration extends DokuWiki_Action_Plugin {
 
@@ -22,22 +21,55 @@ class action_plugin_restrictedregistration extends DokuWiki_Action_Plugin {
      * register the eventhandlers
      */
     public function register(Doku_Event_Handler &$controller) {
-       $controller->register_hook('AUTH_USER_CHANGE', 'BEFORE', $this, 'handle_auth_user_change');
+        $controller->register_hook('AUTH_USER_CHANGE', 'BEFORE', $this, 'handle_auth_user_change');
     }
 
     /**
      * Checks if email address is on the list
      */
     public function handle_auth_user_change(Doku_Event &$event, $param) { 
-       //we are only interested in account registrations
-       //TODO: What about changing the address after successful registration?
-       if ($event->data["type"] == "create") {
-           if (!checkMail($event->data["params"][3])) {
-               $event->preventDefault();
-               $event->stopPropagation();
-               msg($this->getLang("not_allowed_address"), -1);
-           }           
-       }
+        //we are only interested in account registrations
+        //TODO: What about changing the address after successful registration?
+        if ($event->data["type"] == "create") {
+            if (!$this->checkMail($event->data["params"][3])) {
+                $event->preventDefault();
+                $event->stopPropagation();
+                msg($this->getLang("not_allowed_address"), -1);
+            }           
+        }
+    }
+    
+    
+    private function checkMail($mailaddress) {
+        $addresses = $this->getAddressList();
+
+        /*
+         * Allow, if something goes wrong but the fallback action is set to "allow"
+         * or if everything goes well and the entered address is found
+         */
+        if (($addresses == FALSE && $this->getConf("fallback_action") == 1) ||
+            ($addresses != FALSE && in_array($mailaddress, $addresses))) {
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        
+    }
+
+    private function getAddressList() {
+        //Initialize $output
+        unset($output);
+
+        $commandstr=$this->getConf("ezmlm-binary"). " ".$this->getConf("mailinglist-subdir");
+        exec($commandstr, $output, $retval);    //TODO: Replace exec by PHP-based file parsing
+
+        if ($retval != 0) {
+            //Log error
+            dbglog("Execution of `$commandstr` failed with returnvalue $retval and output \"$output\"");
+            return FALSE;
+        }else{
+            return $output;
+        }
     }
 }
 
